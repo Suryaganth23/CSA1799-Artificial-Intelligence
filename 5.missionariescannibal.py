f7072c2e9612f5c684b1ce235cb58dce8d15ac2a
Suryaganth23 <113573802+Suryaganth23@users.noreply.github.com>
2023-11-23 14:41:17 +0530

    Update 5.missionariescannibal.py

--- 5.missionariescannibal.py
+++ 5.missionariescannibal.py
@@ -1,4 +1,3 @@
-print("Murali krishna-192124087")
 print("\n")
 print("\tGame Start\nNow the task is to move all of them to right side of the river")
 print("rules:\n1. The boat can carry at most two people\n2. If cannibals num greater than missionaries then the cannibals would eat the missionaries\n3. The boat cannot cross the river by itself with no people on board")
